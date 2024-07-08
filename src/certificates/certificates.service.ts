812cedab26ce74a1663658e7e26d2ea17fc804a3
kareem <kareem.adel.zayed@gmail.com>
2024-07-08 16:45:22 +0300

    inital

--- src/certificates/certificates.service.ts
+++ src/certificates/certificates.service.ts
@@ -307,7 +307,7 @@ export class CertificatesService {
       color: fontColor,
     });
     page.drawText(data.TrainingCenterName, {
-      x: 375 - data.TrainingCenterName.length * fontSize * 0.4,
+      x: 348,
       y: 745,
       size: fontSize,
       color: fontColor,
