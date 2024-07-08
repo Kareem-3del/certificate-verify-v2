af03dc1e3dc40570f65fc1101ab420772814e2bb
kareem <kareem.adel.zayed@gmail.com>
2024-07-08 14:09:19 +0300

    inital

--- src/certificates/settings/settings.service.ts
+++ src/certificates/settings/settings.service.ts
@@ -2,7 +2,7 @@
 
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Column, Repository } from 'typeorm';
+import { Repository } from 'typeorm';
 import { Settings } from './settings.entity';
 
 @Injectable()
