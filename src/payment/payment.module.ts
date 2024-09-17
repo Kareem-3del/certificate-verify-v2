946872997d5234bfd51d24dd9423c7d3c85d41e9
kareem <kareem.adel.zayed@gmail.com>
2024-09-17 06:49:09 +0300

    Settings & Live Mode

--- src/payment/payment.module.ts
+++ src/payment/payment.module.ts
@@ -8,16 +8,18 @@ import { PaypalService } from './paypal/paypal.service';
 import { PaypalController } from './paypal/paypal.controller';
 import { ConfigModule } from '@nestjs/config';
 import { SubscriptionsModule } from '../subscriptions/subscriptions.module';
+import { Payid19Module } from './payid19/payid19.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forFeature([Transaction]),
     StripeModule.forRootAsync(),
+    Payid19Module,
     SubscriptionsModule,
   ],
   providers: [PaymentService, PaypalService],
-  exports: [PaymentService, StripeModule, PaypalService],
+  exports: [PaymentService, StripeModule, PaypalService, Payid19Module],
   controllers: [PaypalController],
 })
 export class PaymentModule {}
--- src/subscriptions/subscriptions.controller.ts
+++ src/subscriptions/subscriptions.controller.ts
@@ -15,11 +15,15 @@ import { CreateSubscriptionDto } from './dto/create-subscription.dto';
 import { UpdateSubscriptionDto } from './dto/update-subscription.dto';
 import { Response } from 'express';
 import { PaypalService } from '../payment/paypal/paypal.service';
+import { StripeService } from '../payment/stripe/stripe.service';
+import { Payid19Service } from '../payment/payid19/payid19.service';
 @Controller('subscriptions')
 export class SubscriptionsController {
   constructor(
     private readonly subscriptionsService: SubscriptionsService,
     private readonly paypalService: PaypalService,
+    private readonly stripeService: StripeService,
+    private readonly payid19Service: Payid19Service,
   ) {}
 
   @Post()
@@ -75,6 +79,7 @@ export class SubscriptionsController {
     @Body('instructor_id') instructor_id: string,
     @Body('instructor_name') instructor_name: string,
     @Body('center_name') center_name: string,
+    @Body('pay_with') pay_with: string,
     @Res() res: Response,
   ) {
     try {
@@ -83,15 +88,60 @@ export class SubscriptionsController {
       if (!sub) {
         throw new HttpException('Subscription not found', 404);
       }
-      const url = await this.paypalService.createPaymentLink(
-        email,
-        sub.price,
-        sub.id.toString(),
-        sub.name + ' Subscription ' + sub.points + ' Points',
-        instructor_name,
-        instructor_id,
-        center_name,
-      );
+      let url = null;
+      switch (pay_with) {
+        case 'crypto':
+          if (!email) {
+            throw new HttpException('Email is required', 400);
+          }
+          url = await this.payid19Service.createInvoice(
+            sub.price,
+            sub.id.toString(),
+            sub.id,
+            sub.name + ' Subscription ' + sub.points + ' Points',
+            'Subscription for ' +
+              sub.name +
+              ' with ' +
+              instructor_name +
+              ' at ' +
+              center_name,
+            {
+              email,
+              userId: sub.id,
+              subId: sub.id,
+              instructor_id,
+              instructor_name,
+              center_name,
+            },
+          );
+          res.redirect(url);
+          return { sub, error: false, email, url };
+        case 'stripe':
+          if (!email) {
+            throw new HttpException('Email is required', 400);
+          }
+          url = await this.stripeService.createPaymentLink(
+            email,
+            sub.price,
+            sub.id.toString(),
+            sub.name + ' Subscription ' + sub.points + ' Points',
+            instructor_name,
+            instructor_id,
+            center_name,
+          );
+          res.redirect(url);
+          return { sub, error: false, email, url };
+        default:
+          url = await this.paypalService.createPaymentLink(
+            email,
+            sub.price,
+            sub.id.toString(),
+            sub.name + ' Subscription ' + sub.points + ' Points',
+            instructor_name,
+            instructor_id,
+            center_name,
+          );
+      }
       res.redirect(url);
       return { sub, error: false, email, url };
     } catch (e) {
